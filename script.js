afcdcaa10ab589dc77f1b0544c197f9618fe5026
Your Name <you@example.com>
2021-11-25 15:36:14 +0100

    add toogle button and handle error

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ let meteoCity;
 
 //fetch Meteo data
 async function startMeteoApi(city) {
+    try {
     const apiResponse = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=f85db4f81e9fe36af88d71da420865f0', {mode: 'cors'});
+    if(!apiResponse.ok)
+    {
+        console.error('Error status code')
+    }
     const response =  await apiResponse.json();
     console.log(response)
     addPrivateObject(response)
+    selectImg(meteoCity.returnMain())
+    } catch (error) {
+        console.error(error)
+        alert('City not found')
+    }
+    
     
    
     
@@ -25,6 +36,11 @@ input.addEventListener('keyup', (text)=>{
     
 })
 
+const toggle = document.querySelector('.checkbox')
+
+toggle.addEventListener('click', ()=>{
+    displayItem()
+})
 
 
 
@@ -80,25 +96,41 @@ function addPrivateObject(objectToAdd) {
 }
 
 function displayItem() {
+    if(toggle.checked) {
+        const temp = document.querySelector('.temper')
+        temp.textContent = meteoCity.returnTempC()
+        const unity = document.createElement('sup')
+        unity.classList.add('temperaturaC')
+        unity.textContent = '℃'
+        temp.appendChild(unity)
+
+        const fell = document.querySelector('.fells')
+        fell.textContent = "FEELS LIKE: "+meteoCity.returnTempCfells()
+        const unityDefault = document.createElement('sup')
+        unityDefault.textContent = '℃'
+        fell.appendChild(unityDefault)
+    }
+    else{
+        const temp = document.querySelector('.temper')
+        temp.textContent = meteoCity.returnTempF()
+        const unity = document.createElement('sup')
+        unity.classList.add('temperaturaC')
+        unity.textContent = '°F'
+        temp.appendChild(unity)
+
+        const fell = document.querySelector('.fells')
+        fell.textContent = "FEELS LIKE: "+meteoCity.returnTempFfells()
+        const unityDefault = document.createElement('sup')
+        unityDefault.textContent = '°F'
+        fell.appendChild(unityDefault) 
+    }
+
     const clima = document.querySelector('.clima');
     clima.textContent = meteoCity.returnDescription()
 
     const city = document.querySelector('.citta')
     city.textContent = meteoCity.returnLocation()
 
-    const temp = document.querySelector('.temper')
-    temp.textContent = meteoCity.returnTempC()
-    const unity = document.createElement('sup')
-    unity.classList.add('temperaturaC')
-    unity.textContent = '℃'
-    temp.appendChild(unity)
-    
-
-    const fell = document.querySelector('.fells')
-    fell.textContent = "FEELS LIKE: "+meteoCity.returnTempCfells()
-    const unityDefault = document.createElement('sup')
-    unityDefault.textContent = '℃'
-    fell.appendChild(unityDefault)
 
     const wind = document.querySelector('.wind')
     wind.textContent = "WIND: "+meteoCity.returnWind()+' MPH'
@@ -106,4 +138,32 @@ function displayItem() {
     const humidity = document.querySelector('.humidity')
     humidity.textContent = "HUMIDITY: "+meteoCity.returnHumidity()+'%'
 
+}
+
+
+function selectImg(description) {
+    const container = document.querySelector('.container')
+    console.log(description)
+    if(description == 'Clear') {
+        container.style.background = 'linear-gradient(#0093E9, #80D0C7)'
+    }
+    else if(description == 'Clouds') {
+        container.style.background = 'linear-gradient(#739AC3, #E0C3FC)'
+    }
+    else if(description == 'Drizzle') {
+        container.style.background = 'linear-gradient(#0093E9, #80D0C7)'
+    }
+    else if(description == 'Rain') {
+        container.style.background = 'linear-gradient(#5A5A5A, #80D0C7)'
+    }
+    else if(description == 'Thunderstorm') {
+        container.style.background = 'linear-gradient(#2B2929, #32605C)'
+    }
+    else if(description == 'Snow') {
+        container.style.background = 'linear-gradient(#FFFFFF, #ABF7ED)'
+    }
+    else if(description == 'Mist') {
+        container.style.background = 'linear-gradient(#F3F9FF, #CDCDCD)'
+    }
+    
 }
